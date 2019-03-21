ce99956a46f077bed84314ff4d73327526f57ed7
dwmoeller1 <dwmoeller1@aol.com>
2019-03-20 19:59:44 -0500

    GetFileData

--- CoderGirl-SalesList/Program.cs
+++ CoderGirl-SalesList/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CoderGirl_SalesList
 {
@@ -6,7 +9,62 @@ namespace CoderGirl_SalesList
     {
         public static void Main(string[] args)
         {
-            
+            Program program = new Program();
+            program.Run();
+            Console.ReadLine();
+        }
+
+        private void Run()
+        {
+            List<SalesRecord> salesRecords = GetSalesRecordsFromFileData();
+            int countNorthAmerica = GetCountForNorthAmerica(salesRecords);
+            Console.WriteLine(countNorthAmerica);
+        }
+
+        private int GetCountForNorthAmerica(List<SalesRecord> salesRecords)
+        {
+            int count = 0;
+            foreach(SalesRecord record in salesRecords)
+            {
+                if(record.Region == "North America")
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        private List<SalesRecord> GetSalesRecordsFromFileData()
+        {
+            List<SalesRecord> salesRecords = new List<SalesRecord>();
+            bool isFirstRow = true;
+            foreach (string line in File.ReadLines(@"1000 Sales Records.csv"))
+            {
+                if (isFirstRow)
+                {
+                    isFirstRow = false;
+                    continue;
+                }
+
+                SalesRecord salesRecord = CreateSalesRecord(line);
+                salesRecords.Add(salesRecord);
+            }
+            return salesRecords;
+        }
+
+        private SalesRecord CreateSalesRecord(string line)
+        {
+            SalesRecord salesRecord = new SalesRecord();
+            string[] properties = line.Split(",");
+            salesRecord.Region = properties[0];
+            salesRecord.Country = properties[1];
+            salesRecord.ItemType = properties[2];
+            salesRecord.SalesChannel = properties[3];
+            salesRecord.OrderPriority = properties[4];
+            salesRecord.OrderDate = DateTime.Parse(properties[5]);
+
+            return salesRecord;
         }
     }
 }
---
+++ CoderGirl-SalesList/SalesRecord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoderGirl_SalesList
+{
+    public class SalesRecord
+    {
+        public string Region { get; set; }
+        public string Country { get; set; }
+        public string ItemType { get; set; }
+        public string SalesChannel { get; set; }
+        public string OrderPriority { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
