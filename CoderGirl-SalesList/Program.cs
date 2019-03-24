2dfc6c0f7d8f7918892dd432c9661ef24d91529a
dwmoeller1 <dwmoeller1@aol.com>
2019-03-24 17:04:37 -0500

    Stage for tests

--- CoderGirl-SalesList/Program.cs
+++ CoderGirl-SalesList/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CoderGirl_SalesList
 {
@@ -6,7 +10,31 @@ namespace CoderGirl_SalesList
     {
         public static void Main(string[] args)
         {
+            Program program = new Program();
+            program.Run();
+            Console.ReadLine();
+        }
+
+        public void Run()
+        {
+            SalesRecordRepository repo = new SalesRecordRepository();
             
+            using (StreamReader reader = new StreamReader(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\Data\1000 Sales Records.csv"))
+            {
+                string line = reader.ReadLine();
+                while ((line = reader.ReadLine()) != null){
+                    List<string> properties = line.Split(",").ToList();
+                    SalesRecord salesRecord = new SalesRecord();
+                    salesRecord.Region = properties[0];
+                    salesRecord.Country = properties[1];
+                    salesRecord.ItemType = properties[2];
+                    salesRecord.SalesChannel = properties[3];
+                    salesRecord.OrderPriority = properties[4];
+                    salesRecord.OrderDate = DateTime.Parse(properties[5]);
+                    salesRecord.OrderID = Convert.ToInt64(properties[6]);
+                    repo.SalesRecords.Add(salesRecord);
+                }
+            }
         }
     }
 }
---
+++ CoderGirl-SalesList/SalesRecord.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CoderGirl_SalesList
+{
+    public class SalesRecord
+    {
+        public string Region { get; set; }
+        public string Country { get; set; }
+        public string ItemType { get; set; }
+        public string SalesChannel { get; set; }
+        public string OrderPriority { get; set; }
+        public DateTime OrderDate { get; set; }
+        public double OrderID { get; set; }
+        public DateTime ShipDate { get; set; }
+        public int UnitsSold { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal UnitCost { get; set; }
+        public decimal TotalRevenue { get; set; }
+        public decimal TotalCost { get; set; }
+        public decimal TotalProfit { get; set; }
+    }
+}
