4498e5d983a572e170607df95aefca12da4f1530
dwmoeller1 <dwmoeller1@aol.com>
2019-03-25 21:23:03 -0500

    Add interfaces

---
+++ CoderGirl-SalesList/ISalesRecordAdapter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoderGirl_SalesList
+{
+    public interface ISalesRecordAdapter
+    {
+        /// <summary>
+        /// Gets the file with a given path and attempts to parse the lines into a list of 
+        /// SalesRecord items
+        /// </summary>
+        /// <param name="filePath">The path to the csv file</param>
+        /// <param name="header">true if the file includes a header line, else false</param>
+        /// <returns>List of SalesRecord items</returns>
+        List<SalesRecord> GetSalesRecordsFromCsvFile(string filePath, bool header = false);
+    }
+}
