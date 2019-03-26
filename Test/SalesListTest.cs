cd32afada963c223a75334aea0669f0bd053c7fa
dwmoeller1 <dwmoeller1@aol.com>
2019-03-25 20:49:50 -0500

    Add tests

---
+++ Test/SalesListTest.cs
@@ -0,0 +1,15 @@
+using System;
+using CoderGirl_SalesList;
+using Xunit;
+
+namespace Test
+{
+    public class SalesListTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
